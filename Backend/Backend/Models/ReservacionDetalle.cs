f9158f275966096c1e7d20d3c21b0f16a8abdcfa
Patricio Escudero <edparra@outlook.com>
2021-04-24 09:55:27 -0500

    Archivos Excluidos que no se usarán en proyecto.

---
+++ Backend/Backend/Models/ReservacionDetalle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class ReservacionDetalle
+    {
+        public int ReservacionId { get; set; }
+        public int MascotaId { get; set; }
+        public virtual Reservacion Reservacion { get; set; }
+        public virtual Mascota Mascota { get; set; }
+    }
+}
