0583d20d7d7c6790a0bf8c991eef4e0fb488f36e
Patricio Escudero <edparra@outlook.com>
2021-05-21 10:17:22 -0500

    ajustes API

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+
+    
+    return(
+        <footer>Derechos Reservados &copy;</footer>
+    );
+}
+
+export default Footer;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+
+
+export default () =>(
+    <Button color="primary">This is a Bulma Button</Button>
+)
