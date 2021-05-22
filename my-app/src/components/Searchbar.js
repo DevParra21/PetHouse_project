5c13d41af3751d408b7706ff60e0030d2c9ecc68
Patricio Escudero <edparra@outlook.com>
2021-05-22 03:03:58 -0500

    Searchbar

---
+++ my-app/src/components/Searchbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './Searchbar.css';
+
+const Searchbar = () => {
+    return (
+    <div className="columns">
+        <div className="column is-full">
+            
+      <div className="field has-addons">
+        <p class="control">
+            <span class="select">
+                <select>
+                    <option>Nombre</option>
+                    <option>Ciudad</option>
+                    <option>Tipo de Mascota</option>
+                    <option>Precio por noche</option>
+                </select>
+            </span>
+        </p>
+        <p className="control is-expanded">
+          <input className="input" type="text" placeholder="Ciudad, Tipo de Mascota, Costo..." />
+        </p>
+        <p className="control">
+          <button className="button is-link">
+            Búsqueda
+          </button>
+        </p>
+      </div>
+      </div>
+    </div>
+     );
+}
+ 
+export default Searchbar;
