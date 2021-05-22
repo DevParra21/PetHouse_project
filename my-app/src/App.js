7ea22dab8735c42f2e98d33cbfc4b00d2d556e91
Patricio Escudero <edparra@outlook.com>
2021-05-22 03:06:05 -0500

    Componente Principal: TODO: routeo de pantallas/Componentes para navegación por aplicación

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,82 @@
-import React, {Fragment} from 'react';
-
-import Header from './components/Header';
-import Footer  from './components/Footer';
+import React, {Fragment, useState} from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Signup from './components/Signup/Signup';
+import Producto from 'components/Producto/Producto';
+import  Card  from 'components/Card/Card';
+import  Searchbar from 'components/Searchbar';
 
 function App() {
+  
+  //useState
+  const [ productos, guardarProductos ] = useState([
+    {id: 1, nombre: 'Camisa', precio:50 },
+    {id: 2, nombre: 'Camisa2', precio:50 },
+  ]);
+
+  const[hogares, guardarHogar] = useState([
+    { id: 1, 
+      nombre: 'Ave. 2 John K.', 
+      cliente:'Alma Patricia', 
+      descripcion:'Esta es un hogar para mascotas',
+      mascotas:'3',
+      precio:'129.99'
+    },
+    { id: 2, 
+      nombre: 'Ave. 2 John K.', 
+      cliente:'Alma Patricia', 
+      descripcion:'Esta es un hogar para mascotas',
+      mascotas:'2',
+      precio:'129.99'
+    },
+    { id: 3, 
+      nombre: 'Ave. 2 John K.', 
+      cliente:'Alma Patricia', 
+      descripcion:'Esta es otro hogar para mascotas',
+      mascotas:'1',
+      precio:'129.99'
+    },
+    { id: 4, 
+      nombre: 'Ave. 2 John K.', 
+      cliente:'Alma Patricia', 
+      descripcion:'Esta es otro hogar para mascotas',
+      mascotas:'1',
+      precio:'129.99'
+    },
+  ]);
+  
   return (
-    <Fragment>
-      <Header />
+      <Fragment>
+        <Navbar />
+        <div className="container">
+        
+        <div className="titulo">
+          <h1><b>PetHouse</b></h1>
+          <div className="subtitulo"><h2>Cuidamos de tu mejor amigo</h2></div>
+          <div className="subtitulo"><h2>Como si fuera el nuestro</h2></div>
+        </div>
+        <h3>Busca el hogar de tu preferencia</h3>
+        <Searchbar />
+
+        <h3>Nuevos Hogares para tu mascota</h3>
+        <div className="columns">
+          {hogares.map(hogar =>(
+            <Card
+            key={hogar.id}
+            hogar={hogar}/>
+            ))}
+        </div>
+        <h3>Hogares Destacados</h3>
+        <div className="columns">
+          {hogares.map(hogar =>(
+            <Card
+            key={hogar.id}
+            hogar={hogar}/>
+            ))}
+        </div>
+        </div>
+      </Fragment>
       
-      <Footer />
-    </Fragment>
   );
 }
 
