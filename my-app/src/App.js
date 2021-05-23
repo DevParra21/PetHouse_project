da2dffee4257c20639cba9ec40b7fcfafc61ab5e
Patricio Escudero <edparra@outlook.com>
2021-05-23 13:28:29 -0500

    Enrutamiento de componentes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,23 @@
 import React, {Fragment, useState} from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Signup from './components/Signup/Signup';
 import Main from 'components/Main/Main';
 import Homedetail from 'components/HomeDetail/Homedetail';
 import './App.css';
+import Reservation from 'components/reserve/Reservation';
 
 function App() {
   
   return (
       <Fragment>
-        <BrowserRouter>
+        <BrowserRouter basename={'/pethouse'}>
         <Navbar />
-        <Redirect 
-          from="/"
-          to="/home"/>
-          <Switch>
-            <Route 
-              path="/home"
-              component = {Main} />
-            <Route
-              exact
-              path="/signup"
-              render={() => <Signup/>} />
-            <Route
-              path="/details"
-              render={() => <Homedetail/>} />
-          </Switch>
+        <Redirect from="/" to="/home"/>
+            <Route exact path="/home" component = {Main} />
+            <Route exact path="/signup" component={Signup} />
+            <Route path="/details" component={Homedetail} />
+            <Route path="/reservation" component={Reservation}/>
         </BrowserRouter>
       </Fragment>
       
